f60088f5c5c009b412fd1704b1a3b5ea38abd146
jay-java <jayprajapati.tops@gmail.com>
2023-06-07 09:11:22 +0530

    first program added

---
+++ FIRST.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	clrscr();
+	int i = 123;
+	char c = 't';
+	float qw = 3.1;
+	printf("i = %d\n",i);
+	printf("c = %c\n",c);
+	printf("qw = %f\n",qw);
+	printf("hi c language\n");
+	printf("hi c language\t again\n");
+	printf("hi again\n");
+	getch();
+	printf("hello c lang");
+	getch();
+}
